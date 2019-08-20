e70694f80004156eefb51201297a95259b358df4
Olumide <olumideokedusi@gmail.com>
2019-08-20 23:38:24 +0100

    Add functionality for remove note

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const _ = require("lodash");
 const yargs = require("yargs");
 
-const note = require("./notes");
+const notes = require("./notes");
 
 const argv = yargs.argv;
 // const command = process.argv[2];
@@ -11,13 +11,21 @@ console.log("Command:", command);
 console.log("Yargs:", argv);
 
 if (command === "add") {
-  note.addNote(argv.title, argv.body);
+  var note = notes.addNote(argv.title, argv.body);
+  note
+    ? console.log(
+        `Note with title ${note.title} and body ${note.body} added successfully`
+      )
+    : console.log("Note title already exist");
 } else if (command === "list") {
-  note.getAll();
+  notes.getAll();
 } else if (command === "read") {
-  note.readNote(argv.title);
+  notes.readNote(argv.title);
 } else if (command === "remove") {
-  note.removeNote(argv.title);
+  var note = notes.removeNote(argv.title);
+  note
+    ? console.log("Deleted successfully")
+    : console.log("Note title does not exist");
 } else {
   console.log("Command not recognized");
 }
