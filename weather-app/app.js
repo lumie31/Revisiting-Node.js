12ba9711c08702f8535a713310709c8158d3bb63
Olumide <olumideokedusi@gmail.com>
2019-08-23 12:04:23 +0100

    querying the open weather map api

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,19 @@
 const request = require("request");
 
-request(
-  {
-    url: "http://www.google.com",
-    json: true
+var options = {
+  method: "GET",
+  url:
+    "https://community-open-weather-map.p.rapidapi.com/weather?q=lagos,nigeria",
+  headers: {
+    "x-rapidapi-key": "80ceaece30mshc5ee85e97368cddp1f8afejsneef04b5aecfa"
   },
-  (error, response, body) => {
-    console.log(body);
-  }
-);
+  json: true
+};
+
+request(options, function(error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(`Location: ${body.name}, ${body.sys.country}`);
+  console.log(`Longitude: ${body.coord.lon}`);
+  console.log(`Latitude: ${body.coord.lat}`);
+});
