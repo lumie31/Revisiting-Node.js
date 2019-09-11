69de7ce346bda3a5a0a69a5e20dd14f86fda23a7
Olumide <olumideokedusi@gmail.com>
2019-09-11 16:18:45 +0100

    Serve data to UI from backend with templating

--- web-servers/server.js
+++ web-servers/server.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const hbs = require("hbs");
 
 var app = express();
 
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  // res.send("<h1>Hello World!</h1>");
+  res.render("home.hbs", {
+    body: "Welcome to the home page",
+    currentYear: new Date().getUTCFullYear(),
+    pageTitle: "Welcome Home"
+  });
 });
 
-app.listen(3002);
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "Welcome to the About Page",
+    currentYear: new Date().getUTCFullYear()
+  });
+});
+
+app.get("/bad", (req, res) => {
+  res.json({
+    errorMessage: "Unable to fulfil promise, sucker"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server started successfully!");
+});
