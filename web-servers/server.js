dca2e67a3a8ab9769c97b955ec84ff005a2e00d0
Olumide <olumideokedusi@gmail.com>
2019-09-12 09:45:48 +0100

    Implement Express middleware

--- web-servers/server.js
+++ web-servers/server.js
@@ -1,24 +1,50 @@
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
 
 var app = express();
 
+hbs.registerPartials(__dirname + "/views/partials");
+
 app.set("view engine", "hbs");
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now} ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) {
+      console.log("Unable to append to server.log file");
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render("maintenance.hbs");
+// });
+
 app.use(express.static(__dirname + "/public"));
 
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getUTCFullYear();
+});
+
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
 app.get("/", (req, res) => {
   // res.send("<h1>Hello World!</h1>");
   res.render("home.hbs", {
     body: "Welcome to the home page",
-    currentYear: new Date().getUTCFullYear(),
     pageTitle: "Welcome Home"
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about.hbs", {
-    pageTitle: "Welcome to the About Page",
-    currentYear: new Date().getUTCFullYear()
+    pageTitle: "Welcome to the About Page"
   });
 });
 
