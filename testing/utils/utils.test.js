21a48b7dfc54675ab6df38599ff5eaf36fa7aceb
Olumide <olumideokedusi@gmail.com>
2019-09-13 13:36:36 +0100

    Update test suites

--- testing/utils/utils.test.js
+++ testing/utils/utils.test.js
@@ -1,17 +1,44 @@
+const expect = require("expect");
+
 const utils = require("./utils");
 
 it("should add two numbers", () => {
   var result = utils.add(33, 11);
 
-  if (result !== 44) {
-    throw new Error(`Expected 44, but got ${result}`);
-  }
+  expect(result).toBe(44);
 });
 
 it("should square a number", () => {
   var res = utils.square(5);
 
-  if (res !== 25) {
-    throw new Error(`Expected 25, but got ${res}`);
-  }
+  expect(res).toBe(25);
+  // if (res !== 25) {
+  //   throw new Error(`Expected 25, but got ${res}`);
+  // }
+});
+
+// it("should expect some values", () => {
+//   // expect(12).toBe(12);
+//   expect({ name: "Andrew" }).toEqual({ name: "Andrew" });
+//   // expect([2, 3, 4]).toInclude(2);
+//   // expect({
+//   //   name: "Olumide",
+//   //   age: 25,
+//   //   location: "Lagos"
+//   // }).toInclude({
+//   //   age: 23
+//   // });
+// });
+
+//Some methods in the expect library seems to have been deprecated after the got merged into the JEST library, so some of the tests below don't actually work as they should
+
+it("should verify first and last names are set", () => {
+  var user = { location: "Lagos", age: 25 };
+  var result = utils.setName(user, "Olumide Okedusi");
+
+  expect(result).toInclude({
+    firstName: "Olumide",
+    lastName: "Okedusi"
+  });
+  // expect(result).toContain({ firstName: "Olumide" });
 });
