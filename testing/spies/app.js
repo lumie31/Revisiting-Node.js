b10386ccb7bf5daec2af431ed07c7ac6b59515e6
Olumide <olumideokedusi@gmail.com>
2019-09-15 22:21:18 +0100

    Add test to spies file

---
+++ testing/spies/app.js
@@ -0,0 +1,9 @@
+var db = require("./db");
+
+module.exports.handleSignup = (email, password) => {
+  //Check if email already exists
+
+  //Save user to db
+  db.saveUser({ email, password });
+  //Send the welcome email
+};
