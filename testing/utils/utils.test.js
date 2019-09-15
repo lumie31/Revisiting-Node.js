a65be303b56a23c18ab894d62eea621d6a6226cd
Olumide <olumideokedusi@gmail.com>
2019-09-15 20:54:50 +0100

    Basic testing flow

--- testing/utils/utils.test.js
+++ testing/utils/utils.test.js
@@ -2,19 +2,35 @@ const expect = require("expect");
 
 const utils = require("./utils");
 
-it("should add two numbers", () => {
-  var result = utils.add(33, 11);
+describe("Utils", () => {
+  it("should add two numbers", () => {
+    var result = utils.add(33, 11);
 
-  expect(result).toBe(44);
-});
+    expect(result).toBe(44);
+  });
 
-it("should square a number", () => {
-  var res = utils.square(5);
+  it("should async add two numbers", done => {
+    utils.asyncAdd(2, 3, sum => {
+      expect(sum).toBe(5);
+      done();
+    });
+  });
 
-  expect(res).toBe(25);
-  // if (res !== 25) {
-  //   throw new Error(`Expected 25, but got ${res}`);
-  // }
+  it("should square a number", () => {
+    var res = utils.square(5);
+
+    expect(res).toBe(25);
+    // if (res !== 25) {
+    //   throw new Error(`Expected 25, but got ${res}`);
+    // }
+  });
+
+  it("should async square a number", done => {
+    utils.asyncSquare(3, sqr => {
+      expect(sqr).toBe(9);
+      done();
+    });
+  });
 });
 
 // it("should expect some values", () => {
@@ -30,7 +46,7 @@ it("should square a number", () => {
 //   // });
 // });
 
-//Some methods in the expect library seems to have been deprecated after the got merged into the JEST library, so some of the tests below don't actually work as they should
+//Some methods in the expect library seems to have been deprecated after they got merged into the JEST library, so some of the tests below don't actually work as they should
 
 it("should verify first and last names are set", () => {
   var user = { location: "Lagos", age: 25 };
